a3eb61e4079e35c53a083a0683b67c2e58dab2cc
n-pretorious <pretoriousndungu@gmail.com>
2020-10-20 14:54:48 +0300

    add profile and images models

--- instagram/models.py
+++ instagram/models.py
@@ -1,3 +1,93 @@
+# from django.contrib.auth.models import User
 from django.db import models
+from cloudinary.models import CloudinaryField
+from django.core.exceptions import ObjectDoesNotExist
+# import datetime as dt
 
-# Create your models here.
+
+class Profile(models.Model):
+    photo = CloudinaryField('photo')
+    name = models.CharField(max_length=15)
+    userName = models.CharField(max_length=15, null=True)
+    bio = models.CharField(max_length=50, null=True, blank=True)
+    posts = models.PositiveIntegerField(null=True, blank=True)
+    followers = models.PositiveIntegerField(default=0)
+    following = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return self.name
+
+    def saveProfile(self):
+        return self.save()
+
+    def deleteProfile(self):
+        return self.delete()
+
+    @classmethod
+    def getProfileById(cls, pk):
+        try:
+            prlObject = cls.objects.get(id=pk)
+            return prlObject
+        except ObjectDoesNotExist:
+            message = 'Profile does not exist'
+            return message
+
+    @classmethod
+    def updateProfile(cls, pk, data):
+        profileUpdate = cls.getImageById(pk)
+        if profileUpdate:
+            Profile.objects.filter(id=data[pk]).update(
+                photo=data['photo'], name=data['name'], bio=data['bio']
+            )
+            message = 'Image was updated successfully'
+            return message
+        else:
+            message = 'Image does not exist'
+            return message
+
+
+class Image(models.Model):
+    image = CloudinaryField('image', null=True)
+    imageCaption = models.CharField(max_length=30)
+    profile = models.ForeignKey(
+        Profile, on_delete=models.CASCADE, related_name='imageProfile'
+    )
+    # editor = models.ForeignKey(User,on_delete=models.CASCADE)
+    likes = models.PositiveIntegerField(default=0)
+    comments = models.PositiveIntegerField(default=0)
+    pubDate = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.imageCaption
+
+    def saveImage(self):
+        return self.save()
+
+    def deleteImage(self):
+        return self.delete()
+
+    @classmethod
+    def getImageById(cls, pk):
+        try:
+            imgObject = cls.objects.get(id=pk)
+            return imgObject
+        except ObjectDoesNotExist:
+            message = 'Image does not exist'
+            return message
+
+    @classmethod
+    def updateImage(cls, pk, newImage):
+        imageUpdate = cls.getImageById(pk)
+        if imageUpdate:
+            Image.objects.filter(id=pk).update(image=newImage)
+            message = 'Image was updated successfully'
+            return message
+        else:
+            message = 'Image does not exist'
+            return message
+
+    # list images from most recent method
+    @classmethod
+    def timeline(cls):
+        timelineImages = cls.objects.latest('pub_date')
+        return timelineImages
