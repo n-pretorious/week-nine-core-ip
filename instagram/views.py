0c88d563ca55f2d6801460a6f220ff322be9a624
n-pretorious <pretoriousndungu@gmail.com>
2020-10-21 21:47:02 +0300

    add a comment for that posts

--- instagram/views.py
+++ instagram/views.py
@@ -1,6 +1,24 @@
-from django.shortcuts import render
-from django.http  import HttpResponse
+from django.http import request
+from instagram.models import Image
+from django.shortcuts import redirect, render
+from django.http import HttpResponse
+
 
 # Create your views here.
 def home(request):
-    return render(request, 'index.html')
+    allPhotos = Image.objects.all()
+    title = "home"
+    
+    if request.method == "POST":
+      igComment = request.POST.get('comment')
+      print(igComment)
+
+    context = {"images": allPhotos, "title": title}
+
+    return render(request, "index.html", context)
+
+
+# def comment(request):
+#     igComment = request.POST.get("modaltextarea")
+#     print(igComment)
+#     return render(request, "index.html")
