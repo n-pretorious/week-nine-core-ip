3343bdf5aad52a736d0cd1237f4a181d5e0cbe6b
n-pretorious <pretoriousndungu@gmail.com>
2020-10-22 09:36:30 +0300

    add comment model to admin

--- instagram/admin.py
+++ instagram/admin.py
@@ -1,6 +1,7 @@
-from instagram.models import Image, Profile
+from instagram.models import Comment, Image, Profile
 from django.contrib import admin
 
 # Register your models here.
 admin.site.register(Profile)
 admin.site.register(Image)
+admin.site.register(Comment)
